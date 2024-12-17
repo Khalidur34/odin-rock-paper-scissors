33ede687759bcb5085250e33d187f0ac9d25852b
Khalidur34 <khalidur34@gmail.com>
2024-12-18 01:47:34 +1100

    completed the rock, paper, scissor but there are few bugs

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let humanScore = 0;
-let compScore = 0;
 
 function getComputerChoice() {
     let compHand = getRndInteger(1, 3);
@@ -14,16 +12,58 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let humanHand = prompt("Enter Rock Paper Scissor: ");
+    let humanHand = prompt("Enter Rock Paper Scissor: ").toLowerCase();
     switch(humanHand) {
-        case "Rock":
+        case "rock":
             return humanHand;
-        case "Paper":
+        case "paper":
             return humanHand;
-        case "Scissors":
+        case "scissors":
             return humanHand;
         default:
             return "Wrong Input";
+    }
+}
+
+
+
+function playGame() {
+    let humanScore = 0;
+    let compScore = 0;
+    function playRound(getComputerChoice, getHumanChoice) {
+        let compChoice = getComputerChoice().toLowerCase();
+        let humanChoice = getHumanChoice().toLowerCase();
+        let draw = "It's a draw!";
+        let win = "You win!";
+        let lose = "You lose :(";
+        if (compChoice === humanChoice) {
+            return "It's a draw!";
+        } 
+        else if (
+            (compChoice === "rock" && humanChoice === "scissors") ||
+            (compChoice === "scissors" && humanChoice === "paper") ||
+            (compChoice === "paper" && humanChoice === "rock")
+        ) {
+            compScore++;
+            return "You lose :(";
+        } 
+        else {
+            humanScore++;
+            return "You win!";
+        }
+    }
+
+    for(let i = 0; i < 5 ; i++) {
+        console.log(playRound(getComputerChoice, getHumanChoice));
+    }
+    if(humanScore > compScore) {
+        return "You Win";
+    }
+    else if(humanScore < compScore){
+        return "You lose";
+    }
+    else {
+        return "It's a draw";
     }
 }
 
@@ -31,4 +71,4 @@ function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-console.log(getHumanChoice());
+console.log(playGame());
